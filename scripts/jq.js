cd7ea6e4d3f6d6a67c0e4f32a7d0504baba311ad
Stevens, Travis (DS) <Travis.Stevens@adp.com>
2014-08-06 16:35:19 -0700

    made some things appear and disappear, i am HOUDINI

---
+++ scripts/jq.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$("#vehicle_btn").click(function(){
+		$("#splash_page").addClass("hidden");
+		$("#main_page").removeClass("hidden");
+	});
+	
+	$("#employee_btn").click(function() {
+		$("#splash_page").addClass("hidden");
+		$("#main_page").removeClass("hidden");
+	});
+
+});
