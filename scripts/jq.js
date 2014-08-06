9e5f083e30a756f3d48179a8c8b32f9e51b5b299
Stevens, Travis (DS) <Travis.Stevens@adp.com>
2014-08-06 16:45:47 -0700

    I GOT IT WORKING< HURRAY

--- scripts/jq.js
+++ scripts/jq.js
@@ -2,11 +2,15 @@ $(document).ready(function(){
 	$("#vehicle_btn").click(function(){
 		$("#splash_page").addClass("hidden");
 		$("#main_page").removeClass("hidden");
+		$("#vehicle_map_tab").addClass("active");
+		$("#vehicles_map").addClass("active");
 	});
 	
 	$("#employee_btn").click(function() {
 		$("#splash_page").addClass("hidden");
 		$("#main_page").removeClass("hidden");
+		$("#employee_map_tab").addClass("active");
+		$("#employees_map").addClass("active");
 	});
 
 });
