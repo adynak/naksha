0c9e8247d32559a4e196ef3e33da1e2edd905cdb
gigstada <gigstada@onid.orst.edu>
2014-08-06 19:24:40 -0700

    Added filter accordion code

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-var scotchApp = angular.module('scotchApp', ['ngRoute']);
+var scotchApp = angular.module('scotchApp', ['ngRoute','ui.bootstrap']);
 
 	// configure our routes
 	scotchApp.config(function($routeProvider) {
